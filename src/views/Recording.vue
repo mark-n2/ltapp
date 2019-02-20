9f28b3f9be50b519eae2131c852ba0f6c497f81a
k11045 <suguro-mas@kyb.co.jp>
2019-02-20 11:24:53 +0900

    sparklineを追加

--- src/views/Recording.vue
+++ src/views/Recording.vue
@@ -38,14 +38,17 @@
           </v-btn>
           <div class="font-weight-light grey--text title mb-2">Recording Sensors</div>
           <h3 class="display-1 font-weight-light orange--text mb-2">Acceleration</h3>
+          <v-sparkline :value="acc_x" color="primary"></v-sparkline>
           <div class="font-weight-light title mb-2">
-            X: {{acc_x}}
+            X: {{acc_x.slice(-1)[0]}}
           </div>
+          <v-sparkline :value="acc_y" color="green"></v-sparkline>
           <div class="font-weight-light title mb-2">
-            Y: {{acc_y}}
+            Y: {{acc_y.slice(-1)[0]}}
           </div>
+          <v-sparkline :value="acc_z" color="red"></v-sparkline>
           <div class="font-weight-light title mb-2">
-            Z: {{acc_z}}
+            Z: {{acc_z.slice(-1)[0]}}
           </div>
           <h3 class="display-1 font-weight-light orange--text mb-2">Gyroscope</h3>
           <div class="font-weight-light title mb-2">
@@ -68,9 +71,9 @@ export default{
   data () {
     return {
       accelerometer: null,
-      acc_x: 0.0,
-      acc_y: 0.0,
-      acc_z: 0.0,
+      acc_x: [0.0],
+      acc_y: [0.0],
+      acc_z: [0.0],
       gyroscope: null,
       gyro_beta: 0.0,
       gyro_gamma: 0.0,
@@ -79,9 +82,18 @@ export default{
   },
   methods: {
     devicemotion () {
-      this.acc_x = this.accelerometer.x
-      this.acc_y = this.accelerometer.y
-      this.acc_z = this.accelerometer.z
+      this.acc_x.push(this.accelerometer.x)
+      if(this.acc_x.length > 1000){
+        this.acc_x.shift()
+      }
+      this.acc_y.push(this.accelerometer.y)
+      if(this.acc_y.length > 1000){
+        this.acc_y.shift()
+      }
+      this.acc_z.push(this.accelerometer.z)
+      if(this.acc_z.length > 1000){
+        this.acc_z.shift()
+      }
     },
     deviceorientation () {
       this.gyro_beta = this.gyroscope.x
